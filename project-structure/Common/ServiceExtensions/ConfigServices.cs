6eb8ca6475be77cc0ff82a0b4eee63b0e3b4c90b
cuongnq <nqcuong.1710@gmail.com>
2025-01-21 04:53:37 +0700

    Add structure base

---
+++ project-structure/Common/ServiceExtensions/ConfigServices.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+namespace Common.ServiceExtensions;
+
+public class ConfigServices
+{
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services.AddLogging(builder =>
+        {
+            builder.ClearProviders();
+            builder.AddConsole();
+            builder.AddFile("logs/app.log");
+            builder.AddDebug();
+        });
+
+    }
+}
